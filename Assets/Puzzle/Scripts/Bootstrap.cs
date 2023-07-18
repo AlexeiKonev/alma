aa53471159007a352348b51b3b691cef57951aa7
AlexeiKonev <alexkonev777@gmail.com>
2023-07-18 15:46:20 +0300

    before Bootstrapchange

--- Assets/Puzzle/Scripts/Bootstrap.cs
+++ Assets/Puzzle/Scripts/Bootstrap.cs
@@ -1,18 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Bootstrap : MonoBehaviour
 {
-    // Start is called before the first frame update
+   private PuzzleGenerator _puzzleGenerator;
+
+  
+
+
+
     void Start()
     {
-        
+        _puzzleGenerator = new PuzzleGenerator();
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+     if(Input.GetKeyUp(KeyCode.Space))
+        {
+            GeneratePuzzle();
+        }   
+    }
+    public void GeneratePuzzle()
+    {
+        Debug.Log("create puzzle");
     }
 }
